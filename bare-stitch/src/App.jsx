75234cb7e0054a7e75b8ba7cc85d6490e39b4a76
luci <tan.ralphangelo@gmail.com>
2025-08-01 10:43:31 +0800

    testing video background

--- bare-stitch/src/App.jsx
+++ bare-stitch/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import background from 'assets/background.mp4'
 import './App.css'
 
 const Card = ({title}) => {
@@ -12,10 +13,18 @@ const Card = ({title}) => {
 
 const App = () => {
   return(
+    <main>
     <div>
+      <video src=
+{background}
+            autoplay="{true}" loop muted
+            className="absolute z-10 w-auto 
+            min-w-full min-h-full max-w-none">
+        </video>
     <h2 className="text-3xl font-bold underline">Bare Stitch</h2>
     <Card title = "Cute cat"/>
     </div>
+    </main>
   )
 }
 
